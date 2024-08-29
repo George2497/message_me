3c59b07f0a2fb40ba25d0654baa483a5546bcb8f
George Koundouri <georgekoundouri@msn.com>
2024-08-29 21:20:42 +0100

    Add root route, controllers and views for initial commit

---
+++ app/controllers/chatroom_controller.rb
@@ -0,0 +1,18 @@
+class ChatroomController < ApplicationController
+
+  def new
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+end
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,21 @@
+class SessionsController < ApplicationController
+
+  def create
+  end
+
+  def new
+  end
+
+  def index
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+
+end
