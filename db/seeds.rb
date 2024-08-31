211005713f5888225d850c3d152aae7bba909c81
George Koundouri <georgekoundouri@msn.com>
2024-08-31 12:46:09 +0100

    Enable User rescource

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+User.create(username: "George", password: "password")
+User.create(username: "Mario", password: "password")
+User.create(username: "Luigi", password: "password")
+User.create(username: "Wario", password: "password")
+User.create(username: "Waluigi", password: "password")
